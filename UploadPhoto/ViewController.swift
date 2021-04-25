fdcc357c5543bbc8fe00483a09a222cc79e00705
syedmahvish <syedmahvish822@gmail.com>
2021-04-25 16:19:00 -0400

    Image uploaded

--- UploadPhoto/ViewController.swift
+++ UploadPhoto/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var imageViewController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageViewController.delegate = self
     }
 
 
+    @IBAction func selectImage(_ sender: UIButton) {
+        imageViewController.sourceType = .photoLibrary
+        imageViewController.allowsEditing = true
+        present(imageViewController, animated: true, completion: nil)
+    }
 }
 
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            imageView.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
